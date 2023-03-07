0f1bfcd27f4751e03ab9e10651958451ac89573a
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-07 11:23:58 +0100

    header component created

---
+++ src/components/header.component.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="owner-name">adamkeyes</div>
+      <div className="social-media-links"></div>
+    </header>
+  );
+}
