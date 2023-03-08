6212127c99398253af3f77de0113bdf7a0319ebc
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-08 15:21:03 +0100

    projects comp has been created

--- src/components/projects.component.jsx
+++ src/components/projects.component.jsx
@@ -1,23 +1,34 @@
+import projectsData from "../seed.json";
+import { useState } from "react";
+import ProjectImage from "./projectImage.component";
+
 export default function Projects() {
+  const [projects, setProjects] = useState(projectsData);
+
   return (
     <section className="projects">
-      <div>
+      <div className="section-title flex items-center justify-between">
         <h2>Projects</h2>
         <button>Contact me</button>
       </div>
       <div className="project-list">
-        <div className="project">
-          <img className="project-img" src="" alt="" />
-          <div className="project-name">Design portfolio</div>
-          <div className="project-tech">
-            <div className="tech">HTML</div>
-            <div className="tech">Css</div>
-          </div>
-          <div className="project-buttons">
-            <button>View Project</button>
-            <button>View Code</button>
-          </div>
-        </div>
+        {projects.map((project) => {
+          return (
+            <div className="project">
+              <ProjectImage url={project.img} alt={project.title} />
+              <div className="project-name">{project.title}</div>
+              <div className="project-tech flex gap-3">
+                {project.tech.map((tech) => {
+                  return <div className="tech">{tech}</div>;
+                })}
+              </div>
+              <div className="project-buttons flex gap-4">
+                <a href="">View Project</a>
+                <a href="">View Code</a>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
