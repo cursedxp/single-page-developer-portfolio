0079c5dafada2d70dbf0ebfbbf9f89b49a087dc7
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-08 10:57:01 +0100

    profile component has been created

---
+++ src/components/profile.component.jsx
@@ -0,0 +1,21 @@
+import cirle from "../images/pattern-circle.svg";
+import oval from "../images/pattern-rings.svg";
+
+export default function Profile(params) {
+  return (
+    <section className="profile">
+      <img className="oval" src={oval} alt="" />
+      <div className="profile-image">
+        <img className="circle" src={cirle} alt="" />
+      </div>
+      <div className="personal-information flex column items-center gap-5">
+        <h2>Nice to meet you! I'm Adam Keyes.</h2>
+        <p>
+          Based in the UK, I'm a front-end developer passionate about building
+          accessible web apps that users love.
+        </p>
+        <button>Contact me</button>
+      </div>
+    </section>
+  );
+}
