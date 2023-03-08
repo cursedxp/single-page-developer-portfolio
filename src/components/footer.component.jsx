a091aef3b56573a00c38ffd07938566494a5a1e4
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-08 18:31:34 +0100

    footer component has been created

---
+++ src/components/footer.component.jsx
@@ -0,0 +1,35 @@
+//Assets
+import githubIcon from "../images/icon-github.svg";
+import femIcon from "../images/icon-frontend-mentor.svg";
+import linkedInIcon from "../images/icon-linkedin.svg";
+import twitter from "../images/icon-twitter.svg";
+
+export default function Footer() {
+  return (
+    <footer className="flex column justify-center items-center gap-4">
+      <div className="owner-name">adamkeyes</div>
+      <ul className="social-media-links flex items-center gap-4">
+        <li>
+          <a href="" target="_blank">
+            <img src={githubIcon} alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="" target="_blank">
+            <img src={femIcon} alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="" target="_blank">
+            <img src={linkedInIcon} alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="" target="_blank">
+            <img src={twitter} alt="" />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
