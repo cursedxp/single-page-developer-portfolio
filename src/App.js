05b39efdcdc6423b6967c3ddd8e24deffbdba55e
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-08 15:25:10 +0100

    contact added as an component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/header.component";
 import Profile from "./components/profile.component";
 import Skills from "./components/skills.component";
 import Projects from "./components/projects.component";
+import Contact from "./components/contact.component";
 
 function App() {
   return (
@@ -14,22 +15,7 @@ function App() {
         <Profile />
         <Skills />
         <Projects />
-        <section>
-          <div>
-            <h3>Contact</h3>
-            <p>
-              I would love to hear about your project and how I could help.
-              Please fill in the form, and I’ll get back to you as soon as
-              possible.
-            </p>
-          </div>
-          <form action="">
-            <input type="text" name="name" />
-            <input type="email" name="email" />
-            <textarea name="message" id="" cols="30" rows="10"></textarea>
-            <button>Send Messa</button>
-          </form>
-        </section>
+        <Contact />
       </main>
       <footer>
         <div className="owner-name">adamkeyes</div>
