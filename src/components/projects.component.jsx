78a6e0443133cbbb0cb16e83447587124ce0af42
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-08 11:45:37 +0100

    projects component created

---
+++ src/components/projects.component.jsx
@@ -0,0 +1,24 @@
+export default function Projects() {
+  return (
+    <section className="projects">
+      <div>
+        <h2>Projects</h2>
+        <button>Contact me</button>
+      </div>
+      <div className="project-list">
+        <div className="project">
+          <img className="project-img" src="" alt="" />
+          <div className="project-name">Design portfolio</div>
+          <div className="project-tech">
+            <div className="tech">HTML</div>
+            <div className="tech">Css</div>
+          </div>
+          <div className="project-buttons">
+            <button>View Project</button>
+            <button>View Code</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
