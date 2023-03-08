7940f0f9e794ab8be4b26425a42625881ac6abbd
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-08 18:18:25 +0100

    contact component

--- src/components/contact.component.jsx
+++ src/components/contact.component.jsx
@@ -1,19 +1,31 @@
+import oval from "../images/pattern-rings.svg";
+
 export default function Contact() {
   return (
-    <section>
+    <section className="contact">
       <div>
-        <h3>Contact</h3>
+        <h2>Contact</h2>
         <p>
           I would love to hear about your project and how I could help. Please
           fill in the form, and I’ll get back to you as soon as possible.
         </p>
       </div>
       <form action="">
-        <input type="text" name="name" />
-        <input type="email" name="email" />
-        <textarea name="message" id="" cols="30" rows="10"></textarea>
-        <button>Send Messa</button>
+        <input type="text" name="name" placeholder="NAME" />
+        <br />
+        <input type="email" name="email" placeholder="EMAIL" />
+        <textarea
+          name="message"
+          id=""
+          cols="30"
+          rows="10"
+          placeholder="MESSAGE"
+        ></textarea>
+        <div className="flex justify-end">
+          <button>Send Message</button>
+        </div>
       </form>
+      <img className="oval" src={oval} alt="" />
     </section>
   );
 }
