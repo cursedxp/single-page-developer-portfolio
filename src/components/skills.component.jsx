3ce4477f65d72147f69449390aead5f3c39e442d
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-09 14:50:41 +0100

    skill component class name change

--- src/components/skills.component.jsx
+++ src/components/skills.component.jsx
@@ -2,27 +2,27 @@ import oval from "../images/pattern-rings.svg";
 export default function Skills() {
   return (
     <section className="skills">
-      <div>
+      <div className="skill">
         <h3>HTML</h3>
         <p>4 Years Experience</p>
       </div>
-      <div>
+      <div className="skill">
         <h3>CSS</h3>
         <p>4 Years Experience</p>
       </div>
-      <div>
+      <div className="skill">
         <h3>JavaScript</h3>
         <p>4 Years Experience</p>
       </div>
-      <div>
+      <div className="skill">
         <h3>Accessibility</h3>
         <p>4 Years Experience</p>
       </div>
-      <div>
+      <div className="skill">
         <h3>React</h3>
         <p>3 Years Experience</p>
       </div>
-      <div>
+      <div className="skill">
         <h3>Sass</h3>
         <p>3 Years Experience</p>
       </div>
