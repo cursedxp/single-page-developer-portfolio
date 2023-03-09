eab24c3f2f212d852318bfc84a415f4b2acba955
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-09 15:40:45 +0100

    unnecessray div removed from contact comp

--- src/components/contact.component.jsx
+++ src/components/contact.component.jsx
@@ -34,13 +34,11 @@ export default function Contact() {
 
   return (
     <section className="contact">
-      <div>
-        <h2>Contact</h2>
-        <p>
-          I would love to hear about your project and how I could help. Please
-          fill in the form, and I’ll get back to you as soon as possible.
-        </p>
-      </div>
+      <h2>Contact</h2>
+      <p>
+        I would love to hear about your project and how I could help. Please
+        fill in the form, and I’ll get back to you as soon as possible.
+      </p>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
