e914439646634290722b4fde236b0a5e377aa2f2
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-10 17:44:25 +0100

    header comp link fix

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -10,22 +10,22 @@ export default function Header() {
       <div className="owner-name">adamkeyes</div>
       <ul className="social-media-links flex items-center gap-4">
         <li>
-          <a href="" target="_blank">
+          <a href="/" target="_blank">
             <img src={githubIcon} alt="" />
           </a>
         </li>
         <li>
-          <a href="" target="_blank">
+          <a href="/" target="_blank">
             <img src={femIcon} alt="" />
           </a>
         </li>
         <li>
-          <a href="" target="_blank">
+          <a href="/" target="_blank">
             <img src={linkedInIcon} alt="" />
           </a>
         </li>
         <li>
-          <a href="" target="_blank">
+          <a href="/" target="_blank">
             <img src={twitter} alt="" />
           </a>
         </li>
